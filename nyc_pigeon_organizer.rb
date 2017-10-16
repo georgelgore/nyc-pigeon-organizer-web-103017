4a9b8f10f21d08d2f90c84305772f112eab41f40
George Gore <georgelgore@gmail.com>
2017-10-16 20:43:32 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,11 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pidgeon_list = {}
+
+  data.each do |category, hash|
+    hash.each do |info, pidgeon|
+      pidgeon_list[:pidgeon] = {:color => [], :gender => [], :lives => []
+    end
+  end
+  
 end
